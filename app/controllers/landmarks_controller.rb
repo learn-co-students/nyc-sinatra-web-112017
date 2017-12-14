861c38a837bb9ab6a9691f316627804b9fe74cf9
nspp99b <chromepanthers@gmail.com>
2017-12-14 13:11:00 -0500

    created all migrations, classes. Added controllers and figures views, edited rakefile and configru

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,12 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    erb :index
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :show
+  end
+
 end
