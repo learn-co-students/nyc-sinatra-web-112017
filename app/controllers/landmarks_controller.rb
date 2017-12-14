2f1b37d01556b3df1c97878327a5d183a379b597
github username <github email address>
2017-12-14 11:51:39 -0500

    created landmarks view

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,20 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :"landmarks/new"
+  end
+
+  get '/landmarks/:id' do
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    erb :'/landmarks/edit'
+  end
+
 end
