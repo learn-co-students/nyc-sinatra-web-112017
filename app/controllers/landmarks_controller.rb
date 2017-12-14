1df35cfa99abb40a489dfcdddc35a932c0422a7d
mattfaircloth <mattcfaircloth@gmail.com>
2017-12-14 17:52:48 -0500

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,42 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+     erb :"landmarks/index"
+  end
+
+  get '/landmarks/new' do
+     #create form
+     erb :"landmarks/new"
+  end
+
+  get '/landmarks/:id' do
+     @landmark = Landmark.find(params[:id])
+     erb :"landmarks/show"
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/edit"
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  delete "/landmarks/:id" do
+    @landmark = Landmark.find(params[:id])
+    @landmark.destroy()
+
+    redirect "/landmarks"
+  end
+
+
 end
---
+++ db/migrate/20171214165020_create_landmarks.rb
@@ -0,0 +1,9 @@
+class CreateLandmarks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.string :year_completed
+      t.integer :figure_id
+    end
+  end
+end
