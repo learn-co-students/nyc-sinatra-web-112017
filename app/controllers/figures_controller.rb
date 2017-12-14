005a83dc63c6b2fd668929c2d43ad3ddfd39e36a
Matt McAlister <matt.mcalister93@gmail.com>
2017-12-14 13:51:39 -0500

    created add new figures form

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,44 @@
+class FiguresController < ApplicationController
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"figures/new"
+  end
+
+  post "/figures" do
+    @figure = Figure.create(params[:figure])
+    if params[:landmark][:name] != ""
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+    if params[:title][:name] != ""
+      @title =  Title.create(params[:title])
+      @figure.titles << @title
+    end
+    # binding.pry
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    redirect "/figures/#{@figure.id}"
+  end
+
+end
